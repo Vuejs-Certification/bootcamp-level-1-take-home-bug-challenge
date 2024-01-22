55b76cc9283c9261f23d4ade17818665f640fb62
Daniel Kelly <me@danielkelly.io>
2024-01-22 14:37:19 -0600

    init commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+// You do not need to change anything in this file
+import { createApp } from "vue";
+import "@vueschool/browser-devtools";
+import App from "./App.vue";
+import "@/assets/main.css";
+
+createApp(App).mount("#app");
