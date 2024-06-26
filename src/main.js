480f24d2863d085fb0c2dbe273dcb7a4fe6983b9
Daniel Kelly <me@danielkelly.io>
2024-06-26 13:13:25 -0500

    feature: console logs to terminal

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "@/assets/main.css";
+import terminal from "virtual:terminal";
+
+// eslint-disable-next-line
+globalThis.console = terminal;
 
 createApp(App).mount("#app");
